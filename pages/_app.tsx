249f496b9e15fca9d6aa36781018484937202e4b
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-12-29 09:10:03 +0100

    ci:Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Nav, Footer, ScrollToTop } from '../components'
 import { NextSeo, DefaultSeo } from 'next-seo'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
     <NextSeo title='SMARK' />
     <Nav />
     <Component {...pageProps} />
+    <Analytics />
     <Footer />
     <ScrollToTop />
   </>)
